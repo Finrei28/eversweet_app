dbce77731878d61c86d4006d01b546ef5da0f764
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-10-04 19:56:55 +1300

    edited promotions

--- backend/src/lib/promotions.ts
+++ backend/src/lib/promotions.ts
@@ -4,7 +4,8 @@ export const promotions = [
     title: "Buy 2 get 1 half price",
     description:
       "buy 2 mochi bowls with mochi and balls and get the 3rd bowl half price. (Offer is valid per order)",
-    category: "",
-    imagePath: "",
+    category: "Mochi Series",
+    imagePath:
+      "https://res.cloudinary.com/dlqjgl6ju/image/upload/v1743684941/products/w5mifpippgwcz5sec56f.jpg",
   },
 ]
