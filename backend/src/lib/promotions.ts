d14763d85f5560f4c3dc2308dc74f54341b0e90b
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-10-06 01:37:32 +1300

    edited promotions

--- backend/src/lib/promotions.ts
+++ backend/src/lib/promotions.ts
@@ -9,4 +9,13 @@ export const promotions = [
     imagePath:
       "https://res.cloudinary.com/dlqjgl6ju/image/upload/v1743684941/products/w5mifpippgwcz5sec56f.jpg",
   },
+  {
+    id: "2",
+    title: "Buy 5 get 1 free",
+    text1: "buy 5 mochi bowls with mochi and balls and get the 5th bowl free.",
+    text2: " (Offer is valid per order)",
+    category: "Mochi Series",
+    imagePath:
+      "https://res.cloudinary.com/dlqjgl6ju/image/upload/v1743684393/products/be60zdo9sergkdadjm2f.jpg",
+  },
 ]
