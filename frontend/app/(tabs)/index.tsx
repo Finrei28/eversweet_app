e872e7c8261331ffe76c8101dcbd62b1d1e2cfd4
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-10-06 01:35:20 +1300

    edited index.tsx

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -107,60 +107,62 @@ export default function Index() {
               </View>
             ))}
 
-            <View className="border border-gray-200 my-6"></View>
+            <View className="border border-gray-200 mt-8 mb-4" />
           </>
         )}
 
-        <>
-          <Text className="text-4xl font-bold text-primary p-5 mt-4 text-center">
-            Our Dessert Series
-          </Text>
-          {homePageContents?.map((category, index) => {
-            return (
-              <View
-                key={index}
-                className="flex justify-center bg-white mx-10 py-5 my-4 gap-5 rounded-2xl shadow-sm"
+        <Text
+          className={`text-4xl font-bold text-primary p-5 text-center ${
+            promotions?.length === 0 ? "mt-4" : ""
+          }`}
+        >
+          Our Dessert Series
+        </Text>
+        {homePageContents?.map((category, index) => {
+          return (
+            <View
+              key={index}
+              className="flex justify-center bg-white mx-10 py-5 my-4 gap-5 rounded-2xl shadow-sm"
+            >
+              <Text className="text-primary font-bold text-2xl text-center">
+                {category.title}
+              </Text>
+              <Image
+                source={{ uri: category.image }}
+                className="w-full bg-white h-80 mx-auto object-cover rounded-lg mt-2"
+                resizeMode="contain"
+                alt={category.title}
+                accessibilityLabel={category.title}
+                accessibilityHint={`This is an image of ${category.title}`}
+                accessibilityRole="image"
+                accessibilityState={{ selected: true }}
+                accessibilityLabelledBy="category-image"
+              />
+              <TouchableOpacity
+                onPress={() => {
+                  // Handle button press
+                  router.replace(`/menu?categoryParam=${category.category}`)
+                }}
+                accessibilityLabel={`View more for ${category.title}`}
+                accessibilityHint={`Press to view more about ${category.title}`}
+                accessibilityRole="button"
+                accessibilityState={{ selected: false }}
+                accessibilityLabelledBy="view-more-button"
+                className="bg-primary rounded-lg p-3 items-center w-1/2 mt-4 mx-auto"
+                // style={{
+                //   backgroundColor: "#007BFF", // Replace with your desired button color
+                //   padding: 10,
+                //   borderRadius: 5,
+                //   alignItems: "center",
+                // }}
               >
-                <Text className="text-primary font-bold text-2xl text-center">
-                  {category.title}
+                <Text style={{ color: "#FFFFFF", fontWeight: "bold" }}>
+                  View More
                 </Text>
-                <Image
-                  source={{ uri: category.image }}
-                  className="w-full bg-white h-80 mx-auto object-cover rounded-lg mt-2"
-                  resizeMode="contain"
-                  alt={category.title}
-                  accessibilityLabel={category.title}
-                  accessibilityHint={`This is an image of ${category.title}`}
-                  accessibilityRole="image"
-                  accessibilityState={{ selected: true }}
-                  accessibilityLabelledBy="category-image"
-                />
-                <TouchableOpacity
-                  onPress={() => {
-                    // Handle button press
-                    router.replace(`/menu?categoryParam=${category.category}`)
-                  }}
-                  accessibilityLabel={`View more for ${category.title}`}
-                  accessibilityHint={`Press to view more about ${category.title}`}
-                  accessibilityRole="button"
-                  accessibilityState={{ selected: false }}
-                  accessibilityLabelledBy="view-more-button"
-                  className="bg-primary rounded-lg p-3 items-center w-1/2 mt-4 mx-auto"
-                  // style={{
-                  //   backgroundColor: "#007BFF", // Replace with your desired button color
-                  //   padding: 10,
-                  //   borderRadius: 5,
-                  //   alignItems: "center",
-                  // }}
-                >
-                  <Text style={{ color: "#FFFFFF", fontWeight: "bold" }}>
-                    View More
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            )
-          })}
-        </>
+              </TouchableOpacity>
+            </View>
+          )
+        })}
       </ScrollView>
     </View>
   )
