2db509f6051fef9315799c167e2ddb6d85e41d3a
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-12-25 12:37:57 +1300

    edited promotions

--- backend/src/lib/promotions.ts
+++ backend/src/lib/promotions.ts
@@ -4,7 +4,7 @@ export const promotions = [
     title: "Buy 2 get 1 half price",
     text1:
       "buy 2 mochi bowls with mochi and balls and get the 3rd bowl half price.",
-    text2: " (The offer will automatically apply)",
+    text2: " (Add dessert to cart to apply offer)",
     category: "Mochi Series",
     imagePath:
       "https://res.cloudinary.com/dlqjgl6ju/image/upload/v1743684941/products/w5mifpippgwcz5sec56f.jpg",
@@ -13,7 +13,7 @@ export const promotions = [
     id: "2",
     title: "Buy 4 get 1 free",
     text1: "buy 4 mochi bowls with mochi and balls and get the 5th bowl free.",
-    text2: " (The offer will automatically apply)",
+    text2: " (Add dessert to cart to apply offer)",
     category: "Mochi Series",
     imagePath:
       "https://res.cloudinary.com/dlqjgl6ju/image/upload/v1743684393/products/be60zdo9sergkdadjm2f.jpg",
