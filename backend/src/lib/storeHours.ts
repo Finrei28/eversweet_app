816a0abc9ead6e6af71e6cd679780bf9d1cb997a
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-12-25 12:42:40 +1300

    edited store hours

--- backend/src/lib/storeHours.ts
+++ backend/src/lib/storeHours.ts
@@ -1,9 +1,19 @@
+// export const storeHours = {
+//   Monday: ["12:30 PM", "9:30 PM"],
+//   Tuesday: ["12:30 PM", "9:30 PM"],
+//   Wednesday: null,
+//   Thursday: ["12:30 PM", "9:30 PM"],
+//   Friday: ["12:00 PM", "10:00 PM"],
+//   Saturday: ["12:00 PM", "10:00 PM"],
+//   Sunday: ["12:00 PM", "10:00 PM"],
+// }
+
 export const storeHours = {
-  Monday: ["12:30 PM", "9:30 PM"],
-  Tuesday: ["12:30 PM", "9:30 PM"],
+  Monday: [null],
+  Tuesday: [null],
   Wednesday: null,
-  Thursday: ["12:30 PM", "9:30 PM"],
-  Friday: ["12:00 PM", "10:00 PM"],
-  Saturday: ["12:00 PM", "10:00 PM"],
-  Sunday: ["12:00 PM", "10:00 PM"],
+  Thursday: [null],
+  Friday: [null],
+  Saturday: [null],
+  Sunday: [null],
 }
