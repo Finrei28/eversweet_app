a891ede8afd9cbe7bd356a05da6f84d60d5b6e32
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2026-06-29 17:37:14 +1200

    edited leaderboardDetails

--- backend/src/lib/leaderboardDetails.ts
+++ backend/src/lib/leaderboardDetails.ts
@@ -1,5 +1,5 @@
 export const leaderboardDetails = {
-  show: false,
+  show: true,
   description:
     "This is a monthly leaderboard which shows how many loyalty points you have earned this month",
 }
