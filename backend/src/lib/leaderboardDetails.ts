664b2d269b4c83b772ce889522e52f322cf24310
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2026-07-31 13:36:40 +1200

    edited leaderboardDetails

--- backend/src/lib/leaderboardDetails.ts
+++ backend/src/lib/leaderboardDetails.ts
@@ -42,6 +42,6 @@ export const organiseLeaderboardDetails = async () => {
     show: true,
     description:
       "This is a monthly leaderboard which shows how many loyalty points you have earned this month",
-    lastMonthsWinner: "Finlay Wong", // Include the resolved winner data here, this will be the winners first and last name if any.
+    lastMonthsWinner: winner, // Include the resolved winner data here, this will be the winners first and last name if any.
   }
 }
